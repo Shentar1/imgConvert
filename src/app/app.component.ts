6e581c2d46b9820e7086d43a00038746b83c10bf
Shentar1 <90427699+Shentar1@users.noreply.github.com>
2025-02-15 14:40:44 -0600

    link app skeleton to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,41 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl:'./img-preview/img-preview.component.html',
   styleUrl:'./img-preview/img-preview.component.css'
 })
+export class imgPreview{}
+
 @Component({
   selector:'imgPreviewLayers',
   templateUrl:'./img-preview-layers/img-preview-layers.component.html',
-  styleUrl:'./img-layers-preview/img-preview-layers.component.css'
+  styleUrl:'./img-preview-layers/img-preview-layers.component.css'
 })
+export class imgPreviewLayers{};
 @Component({
   selector:'selectedFileSettings',
   templateUrl:'./selected-file-settings/selected-file-settings.component.html',
   styleUrl:'./selected-file-settings/selected-file-settings.component.css'
 })
+export class selectedFileSettings{};
 @Component({
   selector:'selectedFilesSidebar',
   templateUrl:'./selected-files-sidebar/selected-files-sidebar.component.html',
   styleUrl:'./selected-files-sidebar/selected-files-sidebar.component.css'
 })
-
+export class selectedFilesSidebar{};
 @Component({
   selector:'toolbar',
   templateUrl:'./toolbar/toolbar.component.html',
   styleUrl:'./toolbar/toolbar.component.css'
 })
-
+export class toolbar{};
+@Component({
+  selector:'controls',
+  templateUrl:'./controls/controls.component.html',
+  styleUrl:'./controls/controls.component.css'
+})
+export class controls{};
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, toolbar,selectedFileSettings,selectedFilesSidebar,imgPreview,imgPreviewLayers,controls],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
