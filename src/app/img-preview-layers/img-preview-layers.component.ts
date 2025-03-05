92332fa4f17025e50c65f38169ec3ff455acd930
Daniel <suckerpunch1@hotmail.com>
2025-03-05 12:10:15 -0600

    implemented ability to move layers up and down in the list

--- src/app/img-preview-layers/img-preview-layers.component.ts
+++ src/app/img-preview-layers/img-preview-layers.component.ts
@@ -12,4 +12,53 @@ export class ImgPreviewLayersComponent {
   protected appendLayer(e:Element, p:HTMLElement){
     p.innerHTML = e.outerHTML;
   }
+  protected moveUpClicked(e:Event, sender:Element){
+    if(sender.parentElement){
+      let siblings = Array.from(sender.parentElement.getElementsByTagName('div'));
+      let parent = sender.parentElement;
+      console.log(siblings);
+      for(let i = 1; i<siblings.length; i++){
+        if(siblings[i]===sender){
+          let currentCopy = sender;
+          let previousElementCopy = siblings[i-1];
+          siblings[i] = previousElementCopy;
+          siblings[i-1] = currentCopy as HTMLDivElement;
+          siblings.forEach(element => {
+            parent.appendChild(element)
+          });
+        }
+      }
+    }
+  }
+  protected moveDownClicked(e:Event, sender:Element){
+    if(sender.parentElement){
+      let siblings = Array.from(sender.parentElement.getElementsByTagName('div'));
+      let parent = sender.parentElement;
+      console.log(siblings);
+      for(let i = 0; i<siblings.length-1; i++){
+        if(siblings[i]===sender){
+          let currentCopy = sender;
+          let nextElementCopy = siblings[i+1];
+          siblings[i] = nextElementCopy;
+          siblings[i+1] = currentCopy as HTMLDivElement;
+          siblings.forEach(element => {
+            parent.appendChild(element)
+          });
+          i=Number.MAX_SAFE_INTEGER;
+        }
+      }
+    }
+  }
+  protected mergeUpClicked(e:Event, sender:Element){
+
+  }
+  protected mergeDownClicked(e:Event, sender:Element){
+
+  }
+  protected deleteClicked(e:Event, sender:Element){
+    
+  }
+  private refreshSVG(layers:HTMLCollection){
+
+  }
 }
