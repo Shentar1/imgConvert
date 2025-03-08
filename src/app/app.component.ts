2f23ff812ef276cc3d773aa3166c1daf6ff75933
Daniel <suckerpunch1@hotmail.com>
2025-03-08 12:21:57 -0600

    remove unnecessary toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output, viewChildren, ViewChildren} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ToolbarComponent} from './toolbar/toolbar.component'
 import { ControlsComponent } from './controls/controls.component';
 import { SelectedFilesSidebarComponent } from './selected-files-sidebar/selected-files-sidebar.component';
 import { SelectedFileSettingsComponent } from './selected-file-settings/selected-file-settings.component';
@@ -9,7 +8,7 @@ import { ImgPreviewLayersComponent } from './img-preview-layers/img-preview-laye
 import { FileObject } from './Classes/FileObject'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolbarComponent,SelectedFileSettingsComponent,SelectedFilesSidebarComponent,ImgPreviewComponent,ImgPreviewLayersComponent,ControlsComponent],
+  imports: [RouterOutlet, SelectedFileSettingsComponent,SelectedFilesSidebarComponent,ImgPreviewComponent,ImgPreviewLayersComponent,ControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
